82fe65f39c32a704eeeefa09afc132da0e41ef3c
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-10-18 18:44:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import List from './components/List.js';
+
+function App() {
+  return (
+    <div>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import style from "./List.module.css";
+
+export default class List extends Component {
+    render() {
+    return(<div>
+        <ul>
+            <li className={style.listitem}>tea</li>
+            <li className= {style.listitem}>coffee</li>
+            <li className= {style.listitem}>beer</li>
+        </ul>
+    </div>)
+    }
+}
